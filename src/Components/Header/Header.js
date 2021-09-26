defbcbd5d4de52b2017f7e02cc1e9b647025e128
Ashik-Ahammad <ashik19ahammad@gmail.com>
2021-09-26 19:44:46 +0600

    first commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div>
-            <h2>What is your favourite TV show???</h2>
+            <h2>What is your favourite TV show??</h2>
             <img className="tv-img" src={tvshow} alt="tv img" />
             <h2>Watch Tv Shows</h2>
             <p>TV can provide a cheap escape instead of travelling. Exposing yourself to the cultures of the world can be easily done from behind a TV screen. Turn on your netflix pick a season of a tv series and unwind to the beautiful images on the screen.</p>
